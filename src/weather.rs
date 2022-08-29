2749ee27fbac82dfbfda30d006a10517ff16b553
haackett <aodhanelectronic@gmail.com>
2022-08-28 22:15:46 -0400

    Serde for parsing perhaps. Have to handle errors.

--- src/weather.rs
+++ src/weather.rs
@@ -1,8 +1,6 @@
 extern crate reqwest;
 
 use std::fmt;
-
-
 use reqwest::*;
 
 pub const API_URL:&str = "https://api.openweathermap.org/data/2.5/";
@@ -12,6 +10,19 @@ pub enum Weather {
     Clear,
     Rainy,
     Snowy,
+    Unknown,
+}
+
+impl Weather {
+    pub fn from_str(s: &str) -> Self {
+        match s {
+            "Clear" => Weather::Clear,
+            "Raining" => Weather::Rainy,
+            "Snowing" => Weather::Snowy,
+            _ => Weather::Unknown,
+        }
+
+    }
 }
 
 pub struct Location {
@@ -27,6 +38,19 @@ pub async fn get_weather(loc: Location, api_key: &str) -> Result<Weather, > {
         .await?
         .text()
         .await?;
-    println!("{:#?}", resp);
-    Ok(Weather::Snowy)
+
+    let json: serde_json::Value = match serde_json::from_str(&resp){
+        Ok(val) => val,
+        Err(_) => serde_json::from_str("{}").unwrap(), 
+    };
+
+    println!("{:#?}", json);
+
+    let weather_string: String = json.get("weather").unwrap()
+        .get(0).unwrap()
+        .get("main").unwrap().to_string();
+
+    println!("{}", weather_string);
+
+    Ok(Weather::from_str(&weather_string))
 }
